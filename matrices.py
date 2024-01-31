774f912156d9ee37df84ed021eff2ad186f7741f
Louis Sun <xw.louissun@gmail.com>
2024-01-31 19:37:54 +0000

    added matrix inverse methods from ME1

--- matrices.py
+++ matrices.py
@@ -3,8 +3,7 @@ import numpy as np
 def decompose(M) -> ([[float]], [[float]]):
     """
     Decomposes the input matrix M by Helmholtz's theroem and returns the irrotational, and incompressible parts as a tuple.
-
-    Parameters
+Parameters
     ----------
     M : [[float]]
         Matrix to be decomposed
@@ -44,3 +43,20 @@ def gaussSeidel(M, b, tol=1e-5) -> [[float]]:
         if np.all(abs(X_guess - X_buffer) < tol): break
         X_buffer = X_guess.copy()
     return X_guess
+
+
+def matMinor(mat, row, col):
+    rows, cols = len(mat), len(mat[0])
+    return [[mat[i][j] for j in range(cols) if j != col] for i in range(rows) if i != row]
+
+def determinant(mat):
+    assert (N := len(mat)) == len(mat[0])
+    if N == 2:
+        return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]
+    return sum([(-1) ** j * mat[0][j] * determinant(matMinor(mat, 0, j)) for j in range(N)])
+
+def solveSystemByCramer(mat, p):
+    assert (N := len(mat)) == len(mat[0]) and len(p[0]) == 1
+    assert (det := determinant(mat)) != 0
+    replaceColumn = lambda M, q, col: [[M[i][j] if j != col else q[i][0] for j in range(N)] for i in range(N)]
+    return [[determinant(replaceColumn(mat, p, j)) / det] for j in range(N)]
